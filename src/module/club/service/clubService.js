35261cf7c29b3ce48c6aa9b8da10f9e66aaa3373
Martin <martinnovoar2@gmail.com>
2020-08-18 19:04:46 -0300

    clubService setup.

--- src/module/club/service/clubService.js
+++ src/module/club/service/clubService.js
@@ -1,5 +1,21 @@
 module.exports = class ClubService {
   constructor(clubRepository) {
-    this.clubRespository = clubRepository;
+    this.clubRepository = clubRepository;
+  }
+
+  async save(club) {
+    return this.clubRepository.save(club);
+  }
+
+  async delete(club) {
+    return this.clubRepository.delete(club);
+  }
+
+  async getClub(id) {
+    return this.clubRepository.getClub(id);
+  }
+
+  async getClubs() {
+    return this.clubRepository.getClubs();
   }
 };
