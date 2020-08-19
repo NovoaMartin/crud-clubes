7105f6dd43815e4616de16b9eeddd8c2f109d69e
Martin <martinnovoar2@gmail.com>
2020-08-19 00:35:52 -0300

    Implemented base view

--- src/module/club/controller/clubController.js
+++ src/module/club/controller/clubController.js
@@ -8,16 +8,20 @@ class ClubController {
   }
 
   configureRoutes(app) {
-    app.get('/', this.index);
-    app.get(`${this.ROUTE}`, this.index);
-    app.get(`${this.ROUTE}/create`, this.create);
-    app.get(`${this.ROUTE}/update/:team`, this.update);
-    app.get(`${this.ROUTE}/delete/:team`, this.delete);
+    app.get('/', this.index.bind(this));
+    app.get(`${this.ROUTE}`, this.index.bind(this));
+    app.get(`${this.ROUTE}/create`, this.create.bind(this));
+    app.get(`${this.ROUTE}/update/:team`, this.update.bind(this));
+    app.get(`${this.ROUTE}/delete/:team`, this.delete.bind(this));
   }
 
   async index(req, res) {
+    const clubs = await this.clubService.getClubs();
     res.render('base', {
       layout: 'base',
+      data: {
+        clubs,
+      },
     });
   }
 
